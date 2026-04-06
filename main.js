90b684689491371ca3d750016a34db509864e45d
JavoxirDeveloper <otajonovjavoxir8@gmail.com>
2026-04-06 10:13:14 +0500

    first commit

--- main.js
+++ main.js
@@ -2,18 +2,30 @@ let loginForm = document.getElementById("loginForm")
 let input = document.querySelector("#input")
 let addBtn = document.getElementById("add-btn")
 let ul = document.querySelector("#lists")
+let p = document.querySelector("#list")
 
 
 loginForm.addEventListener("submit", (e) => {
+
   e.preventDefault()
-  if (input.value.trim() !== "") {
-    let li = document.createElement("li")
-    let button = document.createElement("button")
-    button.textContent = "🗑️"
-    li.textContent = input.value
-    li.appendChild(button)
-    ul.appendChild(li)
-    input.value = ""
+
+  let value = input.value.trim()
+
+  if (value === "") {
+    list.textContent = "Iltimos to'ldiring..."
+
+    setTimeout(()=> {
+      list.textContent = ""
+    }, 3000)
+
+  } else {
+      let li = document.createElement("li")
+      let button = document.createElement("button")
+      button.textContent = "🗑️"
+      li.textContent = input.value
+      li.appendChild(button)
+      ul.appendChild(li)
+      input.value = ""
   }
 })
 
@@ -22,3 +34,6 @@ ul.addEventListener("click", (e) => {
     e.target.parentElement.remove()
   }
 })
+
+
+
